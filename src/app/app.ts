1894e41ef44c9c95b09047c7c0b00d3b115beb08
Eronini Kelechi <102871076+kelechieronini@users.noreply.github.com>
2026-08-26 12:16:44 +0100

    Add new components for Projects, CTA, and Footer sections; update app structure and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
 import { Hero } from "./components/hero/hero";
+import { Projects } from "./components/projects/projects";
+import { Cta } from "./components/cta/cta";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero],
+  imports: [RouterOutlet, Navbar, Hero, Projects, Cta, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
