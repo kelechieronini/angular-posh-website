a2b032a56f2c77f81a4698040d77517aac2cca93
Eronini Kelechi <102871076+kelechieronini@users.noreply.github.com>
2026-08-26 12:10:09 +0100

    first commit

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,10 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class Navbar {}
